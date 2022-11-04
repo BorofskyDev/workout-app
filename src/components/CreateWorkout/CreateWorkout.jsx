f5540665a78bcf76c8bf873f38bdd75eba36b685
solovyovsdom <joelborofsky@mac.com>
2022-11-04 14:46:46 -0500

    skeleton of create workout complete

--- src/components/CreateWorkout/CreateWorkout.jsx
+++ src/components/CreateWorkout/CreateWorkout.jsx
@@ -1,25 +1,119 @@
-import React, { useState } from 'react'
-import { FaArrowAltCircleRight } from 'react-icons/fa';
+import React, { useState } from 'react';
+import './CreateWorkout.css';
 
 function CreateWorkout() {
-    const [formData, setFormData] = useState({
-        exerciseName: "",
-        sets: "",
-        reps: "",
-        weightType: "",
-        useWeight: true,
-        restPeriod: ""
-    })
+  const [formData, setFormData] = useState({
+    exerciseName: '',
+    sets: '',
+    reps: '',
+    isWeight: true,
+    restPeriod: '',
+    workoutType: '',
+  });
+
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === 'checkbox' ? checked : value,
+      };
+    });
+  }
+
   return (
     <section className="workout">
       <h2 className="workout__title">Create Workout</h2>
       <div className="workout__main">
-        <form  className="workout__form">
-            <input type="text" placeholder="Exercise Name"  className="workout__input" />
+        <form className="workout__form">
+          <div className="workout__box exercise-name">
+            <label htmlFor="exerciseName" className="workout__label">
+              Enter exercise name
+            </label>
+            <input
+              type="text"
+              placeholder="Exercise Name"
+              onChange={handleChange}
+              name="exerciseName"
+              value={formData.exerciseName}
+              className="workout__input"
+            />
+          </div>
+          <div className="workout__box sets">
+            <label htmlFor="sets" className="workout__label">
+              Enter number of sets
+            </label>
+            <input
+              type="number"
+              onChange={handleChange}
+              name="sets"
+              value={formData.value}
+              className="workout__number"
+            />
+          </div>
+          <div className="workout__box reps">
+            <label htmlFor="reps" className="workout__label">
+              Enter number of reps
+            </label>
+            <input
+              type="number"
+              onChange={handleChange}
+              name="reps"
+              value={formData.reps}
+              className="workout__number"
+            />
+          </div>
+          <div className="workout__box weight-type">
+            <label htmlFor="isWeight" className="workout__label">
+              Uncheck box if exercise uses body weight
+            </label>
+            <input
+              type="checkbox"
+              id="isWeight"
+              checked={formData.isWeight}
+              onChange={handleChange}
+              name="isWeight"
+              className="workout__check"
+            />
+          </div>
+          <div className="workout__box rest-period">
+            <label htmlFor="restPeriod" className="workout__label">
+              Enter rest duratio in seconds (e.g. 2 Minutes after exercise would
+              equal 120 for the input)
+            </label>
+            <input
+              type="number"
+              onChange={handleChange}
+              name="restPeriod"
+              value={formData.restPeriod}
+              className="workout__number"
+            />
+          </div>
+          <div className="workout__box workout-type">
+            <label htmlFor="workoutType" className="workout__label">
+              Select Exercise Type
+            </label>
+            <select
+              name="workoutType"
+              id="workoutType"
+              onChange={handleChange}
+              value={formData.workoutType}
+              className="workout__selection"
+            >
+              <option value="upperBody">Upper Body</option>
+              <option value="upperBody">Lower Body</option>
+              <option value="upperBody">Core (Abs)</option>
+              <option value="upperBody">Cardio</option>
+              <option value="upperBody">Other</option>
+            </select>
+          </div>
+          <div className="workout__box submit-btn">
+            <button className="btn">Submit</button>
+          </div>
         </form>
       </div>
     </section>
   );
 }
 
-export default CreateWorkout
+export default CreateWorkout;
